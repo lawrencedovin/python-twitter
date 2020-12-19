6b36f591e7f23510341344ab57bc95aab14212c7
Lawrence Dovin <lawrencedovin@gmail.com>
2020-12-19 16:12:02 -0500

    Added form for posting a tweet

--- app.py
+++ app.py
@@ -4,15 +4,7 @@ import twitter
 import tweepy
 from secrets import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET
 
-# location = '11911 Rocking Horse Rd, Rockville, MD'
-# API_BASE_URL = 'http://www.mapquestapi.com/geocoding/v1/address'
-
-# app = Flask(__name__)
-
-# api = twitter.Api(consumer_key=CONSUMER_KEY,
-#                   consumer_secret=CONSUMER_SECRET,
-#                   access_token_key=ACCESS_TOKEN_KEY,
-#                   access_token_secret=ACCESS_TOKEN_SECRET)
+app = Flask(__name__)
 
 def OAuth():
     try:
@@ -22,29 +14,18 @@ def OAuth():
     except Exception as e:
         return None
 
-oauth = OAuth()
-api = tweepy.API(oauth)
+def post_tweet(tweet):
+    oauth = OAuth()
+    api = tweepy.API(oauth)
 
-api.update_status('Hello, this is a tweet!')
-print('A tweet has been posted')
+    api.update_status(tweet)
 
-# def get_coords(address):
-#     response = requests.get(API_BASE_URL, 
-#                             params={"key": API_SECRET_KEY, "location": address})
-    
-#     data = response.json()
-#     results = data["results"]
-#     latitude = data["results"][0]["locations"][0]["latLng"]["lat"]
-#     longitude = data["results"][0]["locations"][0]["latLng"]["lng"]
-#     coords = {"latitude": latitude, "longitude": longitude}
-#     return coords
+@app.route('/')
+def show_address_form():
+    return render_template("post_tweet.html")
 
-# @app.route('/')
-# def show_address_form():
-#     return render_template("address_form.html")
-
-# @app.route('/geocode')
-# def show_coordinates():
-#     address = request.args["address"]
-#     coords = get_coords(address)
-#     return render_template("address_form.html", coords=coords)
+@app.route('/post_tweet')
+def show_tweet():
+    tweet = request.args["post_tweet"]
+    post_tweet(tweet)
+    return render_template("post_tweet.html", tweet=tweet)
